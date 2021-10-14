14244ace392345fe4a6e86b88a6b6b88069452a4
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2021-10-14 20:10:04 +0100

    added equals method

--- src/main/scala/scratch2/Column2.scala
+++ src/main/scala/scratch2/Column2.scala
@@ -7,9 +7,27 @@ class Column2(val name: String, val func: Function[DataFrame, Function[Seq[Strin
       val schema = df.schema
       val data = df.data
 
-      val idx : Int = schema.zipWithIndex.find((el, idx) => el == name).map((el, idx) => idx).getOrElse(throw new RuntimeException("Can't."))
+      val idx: Int = schema.zipWithIndex.find((el, idx) => el == name).map((el, idx) => idx).getOrElse(throw new RuntimeException("Can't."))
 
       row => row(idx)
+    })
+  }
+
+  def this(name: String, lit: String) = {
+    this(name, df => {
+      row => lit
+    })
+  }
+
+  def equals(col: Column2) : Column2 = {
+
+    val name = s"${this.name} equals ${col.name}"
+
+    new Column2(name, df => {
+      val f1 = this.func(df)
+      val f2 = col.func(df)
+
+      row => (f1(row) == f2(row)).toString
     })
   }
 }
--- src/main/scala/scratch2/Main.scala
+++ src/main/scala/scratch2/Main.scala
@@ -7,45 +7,16 @@ object Main {
     val schema = Seq("a", "b", "c")
     val data = Seq(
       Seq("a1", "b1", "c1"),
-      Seq("a2", "b2", "c2"),
-      Seq("a3", "b3", "c3")
+      Seq("a2", "b2", "b2"),
+      Seq("a3", "a3", "a3")
     )
 
     val df1 = new DataFrame(schema, data);
 
 
-    val df2 = df1.select("a")
-    val df3 = df1.select("b")
-    val df4 = df1.select("a", "b")
-    val df5 = df1.select("b", "c")
-    val df6 = df1.select("c", "a")
-    val df7 = df1.select("a", "b", "c")
-    val df8 = df1.select("c", "b", "a")
+    val df2 = df1.select2(new Column2("c") equals new Column2("b"))
 
-    println(df1.data)
     println(df2.data)
-    println(df3.data)
-    println(df4.data)
-    println(df5.data)
-    println(df6.data)
-    println(df7.data)
-    println(df8.data)
-
-    val dff1 = df1.filter(new Column("a", "a", _ == "a1"))
-    val dff2 = df1.filter(new Column("b", "a", _ == "b1"))
-    val dff3 = df1.filter(new Column("c", "a", _ == "b1"))
-    val dff4 = df6.filter(new Column("a", "a", _ == "a1"))
-
-    println(dff1.data)
-    println(dff2.data)
-    println(dff3.data)
-    println(dff4.data)
-
-
-
-    val cake = df1.select2(new Column2("b"))
-    println(cake.data)
-
   }
 
 }

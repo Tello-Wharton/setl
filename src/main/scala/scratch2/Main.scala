82f889cc8f217edc7b9d61b54a0abd5d9e9ccae6
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2021-10-18 20:59:34 +0100

    multiple sort seems to be working

--- src/main/scala/scratch2/Main.scala
+++ src/main/scala/scratch2/Main.scala
@@ -27,8 +27,25 @@ object Main {
     ))
 
 
-    println(df2.sort($"e").data)
-    println(df2.sort2($"e").data)
+//    println(df2.sort($"e").data)
+//    println(df2.sort2($"e").data)
+
+
+    val source3 = Source.fromResource("example3.csv")
+
+
+    val df3 = csvReader.getDataframe(source3, Seq(
+      ("a", StringType),
+      ("b", StringType),
+      ("c", StringType),
+      ("d", IntegerType),
+      ("e", IntegerType)
+    ))
+
+    println(df3.sort2($"b", $"e").data)
+    println(df3.sort2($"c", $"e").data)
+
+
 
 
   }
